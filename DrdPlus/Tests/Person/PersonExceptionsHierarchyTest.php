25163c202cfb97404cec5fa717e5d6383358e93c
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-17 08:13:51 +0100

    Fixed tests after a library incompatible change

--- DrdPlus/Tests/Person/PersonExceptionsHierarchyTest.php
+++ DrdPlus/Tests/Person/PersonExceptionsHierarchyTest.php
@@ -1,15 +1,21 @@
 <?php
 namespace DrdPlus\Tests\Person;
 
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
 class PersonExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
